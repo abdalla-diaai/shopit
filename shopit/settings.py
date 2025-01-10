103236d2fc665e6e24d5f737e8eb4e2328ef82ec
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-01-10 14:27:14 +0000

    added live site to CORS ALLOWED ORIGINS.

--- shopit/settings.py
+++ shopit/settings.py
@@ -150,6 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "http://localhost:5175",
     "http://127.0.0.1:9000",
+    "https://shopit-frontend-3fhp.onrender.com",
 ]
 
 REST_FRAMEWORK = {
