08b09c8e5ced4778e56c7409fb4172d187ccb97b
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-01-11 15:53:01 +0000

    fixed app name in database.

--- shopit/settings.py
+++ shopit/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "shopit.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/shopit',
         conn_max_age=600
     )
 }
